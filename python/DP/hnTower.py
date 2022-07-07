bca21bcec604c2921140f4b0eafc99c0ef012f0e
wippy <98chitrungnd@gmail.com>
2022-07-07 22:38:45 +0100

    permutation DP

--- python/DP/hnTower.py
+++ python/DP/hnTower.py
@@ -1,2 +1,14 @@
 from StackAndQueue.stack import Stack
 
+def moveDisks(n, origin, buffer, destination):
+    if n <= 0:
+        return
+    moveDisks(n - 1, origin, destination, buffer)
+    moveTop(origin, destination)
+    moveDisks(n-1, buffer, destination, origin)
+
+def moveTop(origin, t):
+    top = origin.pop()
+    t.push(top)
+
+
---
+++ python/DP/permutation.py
@@ -0,0 +1,38 @@
+from itertools import permutations
+
+
+def getPermutations(str):
+    if str == None:
+        return None
+    permutations = []
+    if (len(str) == 0):
+        permutations.append("")
+        return permutations
+    firstChar = str[0]
+    permutations = getPermutations(str[1:])
+    for l in list:
+        for pos in len(l):
+            newPerm = insertCharAt(l, firstChar, pos)
+            permutations.append(newPerm)
+    return permutations
+
+def insertCharAt(word, c, pos):
+    firstSubString = word[0:pos]
+    secondSubString = word[pos:len(word)]
+    return firstSubString + c + secondSubString
+
+#n-1 substrings
+def getPermutations2(remainder):
+    if str == None:
+        return None
+    permutations = []
+    if len(str) == 0:
+        permutations.append("")
+        return permutations
+    for i in range(len(remainder)):
+        begin = remainder[:i]
+        end = remainder[i + 1:]
+        partials = getPermutations2(begin + end)
+        for p in partials:
+            permutations.append(remainder[i] + p)
+    return permutations
